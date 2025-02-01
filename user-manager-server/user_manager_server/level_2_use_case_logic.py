619996f611576737bea44dcd2899bc1b7dd1945a
apedano <pedano.alessandro@gmail.com>
2025-02-01 22:23:02 +0100

    Finished business logic classes

--- user-manager-server/user_manager_server/level_2_use_case_logic.py
+++ user-manager-server/user_manager_server/level_2_use_case_logic.py
@@ -17,40 +17,53 @@ class DeleterI:
     def delete(self, user):
         pass
 
-class UserRegistrator:
+class BaseUserManager:
+    def __init__(self, retriever: RetrieverI):
+        self._protected_retriever = retriever
+
+    def _protected_retrieve_by_username(self, username: string):
+        return self._protected_retriever.retrieve_by_username(username)
+
+class UserRegistrator(BaseUserManager):
 
     def __init__(self, retriever: RetrieverI, adder: AdderI, encryption: Encryption):
-        self.__retriever = retriever
+        super().__init__(retriever)
         self.__adder = adder
         self.__encryption = encryption
 
     def register(self, user: User):
         user.validate()
-        if not self.__retriever.retrieve_by_username(user) is None:
-            raise UserException(f"Cannot register user {user.username} because it exists already")
-        if not self.__retriever.retrieve_by_email(user) is None:
+        existing_user = self._protected_retrieve_by_username(user.username)
+        if not existing_user is None:
+            raise UserException(f"No user found with username: {user.username}")
+        if not self._protected_retriever.retrieve_by_email(user.email) is None:
             raise UserException(f"Cannot register user {user.email} already used")
         user.password = self.__encryption.encrypt(user.password)
         self.__adder.add(user)
 
-class UserAuthenticator:
-    def __init__(self, retriever: RetrieverI, encryption: Encryption):
-        self.__retriever = retriever
-        self.__encryption = Encryption
+class UserAuthenticator(BaseUserManager):
 
+    def __init__(self, retriever: RetrieverI, encryption: Encryption):
+        super().__init__(retriever)
+        self.__encryption = encryption
     def authenticate(self, user: User):
-        retrieved_user: User = self.__retriever.retrieve_by_username(user.username)
+
+        retrieved_user: User = self._protected_retriever.retrieve_by_username(user.username)
         if retrieved_user is None:
-            raise UserException(f"Username {user.username} does not exist")
+            raise UserException(f"Username {user.username} not found")
         if self.__encryption.decrypt(retrieved_user.password) != user.password:
             raise UserException(f"Wrong password for user {user.username} ")
         retrieved_user.password = ""
         return retrieved_user
 
-class UserEliminator:
+class UserEliminator(BaseUserManager):
+
+    def __init__(self, deleter: DeleterI, retriever: RetrieverI):
+        super().__init__(retriever)
+        self.__deleter = deleter
+
     def eliminate(self, user: User):
-        pass
-
-
-
-
+        retrieved_user: User = self._protected_retriever.retrieve_by_username(user.username)
+        if retrieved_user is None:
+            raise UserException(f"Username {user.username} does not exist")
+        self.__deleter.delete(user)
